0627b81b8802444d0b39aa7edb165865dc42c825
Christophe THEPAUT <christophe_thepaut@hotmail.fr>
2020-05-20 14:53:09 +0200

    csharp

---
+++ csharp/video-store/VideoStore/Rental.cs
@@ -0,0 +1,14 @@
+namespace VideoStore
+{
+    public class Rental
+    {
+        public int DaysRented { get; }
+        public virtual Movie Movie { get; }
+
+        public Rental(Movie movie, int daysRented)
+        {
+            Movie = movie;
+            DaysRented = daysRented;
+        }
+    }
+}
